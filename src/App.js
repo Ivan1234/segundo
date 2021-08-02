b9ecc279472c736326bde149dd075462dc2daf94
Ivan1234 <raam.rmi95@gmail.com>
2021-08-01 20:45:35 -0500

    Props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Saludar from './components/Saludar';
 
 function App() {
   return (
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Saludar name="Iván Ramírez" edad="25"/>
+          <Saludar name="José" edad="29"/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
