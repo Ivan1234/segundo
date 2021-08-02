c81b66af4252225deb40c5ae5343d8ce6d6e9390
Ivan1234 <raam.rmi95@gmail.com>
2021-08-02 14:50:19 -0500

    Hook de efecto

--- src/components/HookEstado.js
+++ src/components/HookEstado.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function HookEstado(){
 
     const [stateCar, setStateCar] = useState(false);
+    const [contar, setContar] = useState(0);
+
+    useEffect(() => {
+        alert(`Clicks: ${contar}`);
+    }, [contar]);
 
     const encenderApagar = () => {
 
         // setStateCar(!stateCar);
         setStateCar(prevValue => !prevValue);
+        setContar(contar + 1);
 
     };
 
@@ -16,11 +22,10 @@ export default function HookEstado(){
         <div>
 
             <h3>El botón esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+            <h4>Clicks: {contar}</h4>
             <button onClick={encenderApagar}>Encender/Apagar</button>
 
         </div>
-
-        
 
     );
 
